915a7d9dcadb89a10940b52c6681679d0af25d8f
João Vitor Almeida <jvitor.almeida98@gmail.com>
2022-03-29 17:53:05 -0300

    tentando evitar conflitos

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import LoginController from '../Screens/Login/LoginController'
-import HomeController from '../Screens/Home2/HomeController'
-import Home from '../Screens/Home/Home'
+import { Routes, Route } from 'react-router-dom';
+import LoginController from '../Screens/Login/LoginController';
+import HomeController from '../Screens/Home/HomeController';
+import Home from '../Screens/NewHome/Home';
 
 const routes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginController />} />
-      <Route path="/Wallet" element={<HomeController />} />
-      <Route path="/Home" element={<Home />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<LoginController />} />
+			<Route path="/Wallet" element={<HomeController />} />
+			<Route path="/Home" element={<Home />} />
+		</Routes>
+	);
 };
 
-export default routes
+export default routes;
---
+++ src/Screens/NewHome/HomeView.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './Home.css';
+import walletImage from '../../Assets/wallet2.png';
+
+const HomeView = () => {
+	return (
+		<div className="h-screen flex flex-col xl:flex-row">
+			<div
+				id="HomeMessage"
+				className="bg-blue-500 h-3/4 w-screen flex flex-col justify-center items-center xl:h-screen xl:w-2/4"
+			>
+				<span className="text-white text-center mb-8 font-bold text-xl mt-28 ml-2 mr-2 xl:text-2xl">
+					Seja bem-vindo à sua carteira digital!
+				</span>
+				<span className="text-white text-center font-bold text-xl ml-2 mr-2 xl:text-2xl">
+					Aqui você compra em suas lojas favoritas e ganha na hora cashback em sua
+					carteira!
+				</span>
+				<img className="mt-24 w-16 h-16" src={walletImage} alt="Logo de um carteira" />
+			</div>
+			<div
+				id="Buttons"
+				className="bg-slate-200 h-1/4 flex justify-center items-center flex-col xl:h-screen xl:w-2/4"
+			>
+				<button className="w-3/4 bg-white rounded-xl h-10 text-xl font-medium mb-6 xl:h-16 xl:font-normal xl:text-xl">
+					Já sou cadastrado
+				</button>
+				<button className="w-3/4 bg-yellow-500 rounded-xl text-xl font-semibold text-white h-10 xl:font-bold xl:text-xl xl:h-16">
+					Quero me cadastrar
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default HomeView;
