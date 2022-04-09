94adb80f918e030ff0dd51c28fae65754ed7b363
João Vitor Almeida <jvitor.almeida98@gmail.com>
2022-04-09 12:18:19 -0300

    adicionando função esconder o saldo

--- src/Components/SaldoCarteira/SaldoCarteira.js
+++ src/Components/SaldoCarteira/SaldoCarteira.js
@@ -2,12 +2,20 @@ import { Fragment, useRef, useState } from 'react';
 import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline';
 
 export default function Saldo() {
+	const [isVisible, setIsVisible] = useState(false);
 	return (
 		<>
-			<p className="text-xs flex items-center dark:text-white font-semibold ">Acumulado R$</p>
-			<div className="text-md flex items-center text-black font-extrabold">100,00</div>
+			<p className="text-xs flex items-center dark:text-white font-semibold ">
+				Acumulado R$
+			</p>
+			<div className="text-md flex items-center text-black font-extrabold">
+				{isVisible ? '100,00' : '***'}
+			</div>
 			<div className="flex items-center">
-				<button className="text-sm p-1 text-gray-800 mr-4">
+				<button
+					className="text-sm p-1 text-gray-800 mr-4"
+					onClick={() => setIsVisible(!isVisible)}
+				>
 					<EyeIcon className="ml-1 mr-1 h-5 w-5" aria-hidden="true" />
 				</button>
 			</div>
